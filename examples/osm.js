a08e674c255ed6c53161089d7a5b32ee21528716
Frederic Junod <frederic.junod@camptocamp.com>
2012-03-06 14:24:48 +0100

    minimal OSM example

---
+++ examples/osm.js
@@ -0,0 +1,34 @@
+Ext.Loader.setConfig({
+    enabled: true
+});
+
+Ext.Loader.setPath({
+    'GeoExt': '../src'
+});
+
+Ext.application({
+    name: 'GeoExt.example.osm',
+    requires: ['GeoExt.Map'],
+
+    launch: function() {
+        Ext.create('Ext.Panel', {
+            fullscreen: true,
+            layout: 'fit',
+            items: [{
+                xtype: 'toolbar',
+                docked: 'top'
+            }, {
+                xtype: 'gx_map',
+                mapOptions: {
+                    theme: false,
+                    layers: [new OpenLayers.Layer.OSM()]
+                },
+                listeners: {
+                    longpress: function(map, evt) {
+                        alert(evt.lonlat.toString());
+                    }
+                }
+            }]
+        });
+    }
+});
